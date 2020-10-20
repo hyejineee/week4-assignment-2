ad8d404b7469b2f1d2afd85760369adb593a8885
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-20 23:50:49 +0900

    Implement Input component

---
+++ src/Input.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const placeholderText = {
+  name: '이름',
+  category: '분류',
+  address: '주소',
+};
+
+export default function Input({ name, value, onChange }) {
+  function handleChange(event) {
+    const { target } = event;
+    onChange(target.name, target.eventvalue);
+  }
+
+  return (
+    <input
+      type="text"
+      placeholder={placeholderText[name]}
+      name={name}
+      value={value}
+      onChange={handleChange}
+    />
+  );
+}
